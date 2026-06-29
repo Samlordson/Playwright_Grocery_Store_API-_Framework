9f43d80727e68dec7779a012a28aaaa997ccb7f9
SAMLORDSON <samlordson96@gmail.com>
2026-06-29 09:43:30 +0530

    feat: initialize Playwright API automation framework

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: Number(process.env.API_TIMEOUT),
+
+  use: {
+    baseURL: process.env.BASE_URL,
+    extraHTTPHeaders: {
+      Accept: 'application/json'
+    }
+  },
+
+  reporter: [
+    ['list']
+  ]
+});
